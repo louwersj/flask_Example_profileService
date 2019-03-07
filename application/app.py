851288ad5484af55f64eb37439b8ebe595ed4b71
Johan Louwers <louwersj@gmail.com>
2019-03-07 08:55:38 +0100

    cleanup old code and adding proper comments to app.py

--- application/app.py
+++ application/app.py
@@ -5,17 +5,19 @@ from flask import send_from_directory
 app = Flask(__name__)
 
 
+# add a route for / as the root of the application
 @app.route("/")
 def hello():
     return "Hello World!"
 
+
+# add a route for /favicon.ico located in ./static/ to ensure we do not have a 404 on favicon.
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-#app.add_url_rule('/favicon.ico',
-#                 redirect_to=url_for('static', filename='favicon.ico'))
+
 
 if __name__ == "__main__":
     app.run()
