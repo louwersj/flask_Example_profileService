88ee90920d6afc866c3878c4f8876a9333449eec
Johan Louwers <louwersj@gmail.com>
2019-03-07 08:53:54 +0100

    moved favicon.ico to /static and added app.route for favicon. Preventing a 404 on favicon

--- application/app.py
+++ application/app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from flask import send_from_directory
 
 app = Flask(__name__)
 
@@ -7,6 +9,13 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+#app.add_url_rule('/favicon.ico',
+#                 redirect_to=url_for('static', filename='favicon.ico'))
 
 if __name__ == "__main__":
     app.run()
